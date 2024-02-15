10d050c38f90323af5d39460704809a69a889c29
AbhigyanSrivastava <58912311+AbhigyanSrivastava@users.noreply.github.com>
2024-02-15 18:58:33 +0530

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Abhigyan Srivastava",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Facts I Just Made Up",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "Definitly Not Lying",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
