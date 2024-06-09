59980c54f5db94d3fbf076a34788846dbfe7a361
AbhigyanSrivastava <58912311+AbhigyanSrivastava@users.noreply.github.com>
2024-06-09 13:17:52 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -117,7 +117,7 @@ export default {
 		plugin(function ({ addComponents }) {
 			addComponents({
 				".cactus-link": {
-					"@apply bg-[size:100%_6px] bg-bottom bg-repeat-x": {},
+					"@apply bg-[size:100%_6px] bg-bottom bg-repeat-x" no-underline: {},
 					backgroundImage:
 						"linear-gradient(transparent,transparent 5px,hsl(var(--theme-text)) 5px,hsl(var(--theme-text)))",
 					"&:hover": {
@@ -126,7 +126,7 @@ export default {
 					},
 				},
 				".title": {
-					"@apply no-underline text-2xl font-semibold text-accent-2": {},
+					"@apply text-2xl font-semibold text-accent-2" no-underline: {},
 				},
 			});
 		}),
