088fbc69544c5e9a085fae7c42acf08aa6e4e10b
AbhigyanSrivastava <58912311+AbhigyanSrivastava@users.noreply.github.com>
2024-09-05 15:37:24 +0530

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -4,14 +4,6 @@ import type { AstroExpressiveCodeOptions } from "astro-expressive-code";
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Abhigyan Srivastava",
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Facts I Made Up!",
-	// Meta property used as the default description meta property
-	description: "Definitly Not Lying",
-	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
-	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
 		locale: "en-GB",
@@ -21,34 +13,56 @@ export const siteConfig: SiteConfig = {
 			year: "numeric",
 		},
 	},
-
+	// Meta property used as the default description meta property
+	description: "Definitely Not Lying.",
+	// HTML lang property, found in src/layouts/Base.astro L:18
+	lang: "en-GB",
+	// Meta property, found in src/components/BaseHead.astro L:42
+	ogLocale: "en_GB",
+	// Option to sort posts by updatedDate if set to true (if property exists). Default (false) will sort by publishDate
+	sortPostsByUpdatedDate: false,
+	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+	title: "Facts I Made Up!",
+	webmentions: {
+		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-cactus.chriswilliams.dev/posts/webmentions/
+		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
 	},
 };
 
 // Used to generate links in both the Header & Footer.
-export const menuLinks: Array<{ title: string; path: string }> = [
+export const menuLinks: { path: string; title: string }[] = [
 	{
-		title: "Home",
 		path: "/",
+		title: "Home",
 	},
 	{
-		title: "Blog",
 		path: "/posts/",
+		title: "Blog",
 	},
-	{
-		title: "About",
-		path: "/about/",
-	},
-  {
-		title: "Explore",
+  	{
 		path: "/explore/",
+		title: "Explore",
+	},
+  	{
+		path: "/about/",
+		title: "About",
 	},
 ];
 
 // https://expressive-code.com/reference/configuration/
 export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
-	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
-	themes: ["dracula", "github-light"],
+	styleOverrides: {
+		borderRadius: "4px",
+		codeFontFamily:
+			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
+		codeFontSize: "0.875rem",
+		codeLineHeight: "1.7142857rem",
+		codePaddingInline: "1rem",
+		frames: {
+			frameBoxShadowCssValue: "none",
+		},
+		uiLineHeight: "inherit",
+	},
 	themeCssSelector(theme, { styleVariants }) {
 		// If one dark and one light theme are available
 		// generate theme CSS selectors compatible with cactus-theme dark mode switch
@@ -60,17 +74,7 @@ export const expressiveCodeOptions: AstroExpressiveCodeOptions = {
 		// return default selector
 		return `[data-theme="${theme.name}"]`;
 	},
+	// One dark, one light theme => https://expressive-code.com/guides/themes/#available-themes
+	themes: ["dracula", "github-light"],
 	useThemedScrollbars: false,
-	styleOverrides: {
-		frames: {
-			frameBoxShadowCssValue: "none",
-		},
-		uiLineHeight: "inherit",
-		codeFontSize: "0.875rem",
-		codeLineHeight: "1.7142857rem",
-		borderRadius: "4px",
-		codePaddingInline: "1rem",
-		codeFontFamily:
-			'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
-	},
 };
