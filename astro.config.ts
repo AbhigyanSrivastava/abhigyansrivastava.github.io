0bf1a5a90f4194c3df5f8836254e266da82df475
AbhigyanSrivastava <58912311+AbhigyanSrivastava@users.noreply.github.com>
2025-02-26 23:26:34 +0530

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import fs from "fs";
 import mdx from "@astrojs/mdx";
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"; 
 import sitemap from "@astrojs/sitemap";
 import remarkUnwrapImages from "remark-unwrap-images";
 import rehypeExternalLinks from "rehype-external-links";
